7a329060c801f7396814ccfe2cfb0d6c0e7643e3
xeviar <weiwang.sg2@gmail.com>
2022-01-31 17:23:42 +0800

    Update Helper.go
    hello

--- helper/Helper.go
+++ helper/Helper.go
@@ -15,7 +15,7 @@ type SomeStruct struct {
 func RandomNumber(n int) int {
 	rand.Seed(time.Now().Unix())
 	value := rand.Intn(n)
-	fmt.Printf("return %d", value)
+	fmt.Printf("return2222 %d", value)
 
 	return value
 }
